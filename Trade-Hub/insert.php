96da4fbc7a8868698e93828f5ea625f3b78a99db
PatiLetsPlay <PatiLetsPlay77@hotmail.com>
2022-05-11 15:08:36 +0200

    a

--- Trade-Hub/insert.php
+++ Trade-Hub/insert.php
@@ -4,7 +4,21 @@
     <title>Am Server</title>
     </head>
     <body>
+    
     <?php
+        session_start();
+        $_SESSION["a_id"]=$account_id;
+        $_SESSION["a_u"]=$account_username;
+        $_SESSION["a_p"]=$account_password;
+        $_SESSION["a_e"]=$account_email;
+        
+        echo $_SESSION["a_id"];
+        echo $_SESSION["a_u"];
+        echo $_SESSION["a_p"];
+        echo $_SESSION["a_e"];
+
+        
+        
         $name = $_POST["us"];
         $pw = $_POST["pw"];
         $email = $_POST["em"]; 
